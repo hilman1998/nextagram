de2b023b564ddd8271ccaec7d38f0ead222ce70f
hilman <hilmanbinzurin@gmail.com>
2020-07-14 17:22:16 +0800

    First Commit

---
+++ src/pages/UploadPage.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const UploadPage = () => {
+  const [imageFile, setImageFile] = useState(null)
+
+  return (
+      // Your code will go here
+    )
+}
+
+export default UploadPage;
